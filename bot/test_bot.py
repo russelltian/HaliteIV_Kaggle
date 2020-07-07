33ad0627c5c97664bf6b12158454f9f69c4552d1
russell tian <russell.tian1996@gmail.com>
2020-07-07 16:09:36 -0400

    add bot training file, add 650 score benchmark agent, add first bersion of AI bot

---
+++ bot/test_bot.py
@@ -0,0 +1,16 @@
+from kaggle_environments import evaluate, make
+import sys
+from bot import v1_bot
+env = make("halite", configuration={"size": 6, "episodeSteps": 20}, debug=True)
+trainer = env.train([None, "random"])
+observation = trainer.reset()
+while not env.done:
+    mybot = v1_bot.Gameplay(observation,env.configuration)
+    action = mybot.agent(observation, env.configuration)
+    print("my action", action)
+    observation = trainer.step(action)[0]
+    print("Reward gained", observation.players[0][0])
+out = env.render(mode="html", width=500, height=450)
+f = open("replay.html", "w")
+f.write(out)
+f.close()
