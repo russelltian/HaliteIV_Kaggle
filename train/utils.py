28d107e3c0c1fdd30e6474c87affcf7a543f50da
russell tian <russell.tian1996@gmail.com>
2020-07-23 18:02:53 -0400

    Further rework on Gamplay to make it reusable

--- train/utils.py
+++ train/utils.py
@@ -7,27 +7,91 @@ import numpy as np
 
 sys.path.append("../")
 from train import geometry
+
+
 class Gameplay(object):
+    """
+    The base class that supports the agent in real game environment.
+    This class takes the same input as the agent does in real game,
+    and provisions data processing utilities to convert the data to the format
+    that will be fed into our ML based agent.
+    Further optimization and rework can be done by deriving this class.
+    """
+
     def __init__(self, obs, config):
-        size = config["size"]
-        self.size = size
-        self.ships = np.zeros((size, size))
-        self.shipyards = np.zeros((size, size))
         self.board = Board(obs, config)
-        self.me = self.board.current_player.id
+        self.obs = obs
+        size = config["size"]
+        self.board_size = size
+        self.current_player_id = self.board.current_player_id
+
+        self.halite_map = np.zeros((size, size), np.float)  # 2D map of halite
+        self.my_ships_location = np.zeros((size, size), np.int)  # 2D map of my ships location
+        self.my_cargo = np.zeros((size, size), np.float)  # 2D map of my ships cargo
+        self.my_shipyards_location = np.zeros((size, size), np.int)  # 2D map of my shipyards location
+
+        # Store information
+        self.get_ships_information()
+        self.get_halite()
+        self.normalize()
+
+    def get_ships_information(self):
+        """
+        Get current player's ship location, shipyard location, and cargo on the ship
+        :return:
+        """
+        # this_turn.print_board()
+        board = self.board
+        size = self.board_size
+        current_player = board.current_player
+        for ship in current_player.ships:
+            position = self.convert_kaggle_2D_to_coordinate_2D(size, list(ship.position))
+            self.my_ships_location[position[0]][position[1]] = 1
+            self.my_cargo[position[0]][position[1]] = ship.halite
+        for shipyard in current_player.shipyards:
+            position = self.convert_kaggle_2D_to_coordinate_2D(size, list(shipyard.position))
+            self.my_shipyards_location[position[0]][position[1]] = 2
+
+    def get_halite(self):
+        """
+        Get halite
+        :return:
+        """
+        size = self.board_size
+        for i in range(size):
+            for j in range(size):
+                self.halite_map[i][j] = self.obs.halite[i * size + j]
+
+    def normalize(self):
+        """
+        Normalize based on the need
+        :return:
+        """
+        self.halite_map = self.halite_map/100
+        self.my_cargo = self.my_cargo/100
 
     def print_board(self):
-        board = self.board
-        print(board)
+        print(self.board)
+
     def reset_board(self, obs, config):
+        """
+        Reset board information
+        :param obs:
+        :param config:
+        :return:
+        """
         # The size of the board must be constant
-        assert(self.size == config.size)
-        size = self.size
-        self.ships = np.zeros((size, size))
-        self.shipyards = np.zeros((size, size))
+        assert (self.board_size == config["size"])
         self.board = Board(obs, config)
-        # The new observation must still serve for the same player
-        assert(self.me == self.board.current_player.id)
+        self.obs = obs
+        # The new observation must still serve the same player
+        assert (self.current_player_id == self.board.current_player_id)
+
+        # Store information
+        self.get_ships_information()
+        self.get_halite()
+        self.normalize()
+
     # Converts position from 1D to 2D representation in (left most col, left most row)
     def get_col_row(self, size: int, pos: int):
         return pos % size, pos // size
@@ -39,10 +103,12 @@ class Gameplay(object):
         return top_left_row, top_left_col
 
     def test_get_2D_col_row(self, size=21):
-        assert (self.get_2D_col_row(size, 0) == (0, 0))
-        assert (self.get_2D_col_row(size, 10) == (0, 10))
-        assert (self.get_2D_col_row(size, 413) == (19, 14))
-        assert (self.get_2D_col_row(size, 440) == (20, 20))
+        total = 0
+        for i in range(size):
+            for j in range(size):
+                assert (self.get_2D_col_row(size, total) == (i, j))
+                total += 1
+        assert (total == size ** 2)
 
     def get_to_pos(self, size: int, pos: int, direction: str):
         col, row = self.get_col_row(size, pos)
@@ -83,14 +149,22 @@ class Gameplay(object):
         col = pos[0]
         assert (0 <= row < size)
         assert (0 <= col < size)
-        return row*size + col
+        return row * size + col
+
 
 class HaliteV2(object):
+    """
+    This is the wrapper class for preparing training data for ML training mode with Json files.
+    It reads the Json file and convert the information to useful meta data and a list of
+    "Gameplay" object that represents single step of the game, all the core data processing is done in Gameplay,
+    and this class will fetch and reformat the data within "Gameplay" to make it training ready.
+    """
+
     def __init__(self, path: str):
         self.replay, self.config = self.load_replay(path)
         self.total_turns = self.find_total_turns()
         self.winner_id = self.find_winner()
-        self.game_play_list = []
+        self.game_play_list = self.build_game_play_list()
         # halite per cell
         self.halite = []
         self.ship_actions = []
@@ -115,6 +189,11 @@ class HaliteV2(object):
         return replay, config
 
     def find_total_turns(self):
+        """
+        Find how many turns this game has, note the game terminates when there is only one player
+        left, so the game does not have to last for full turns
+        :return:
+        """
         assert (self.config is not None)
         episode_steps = self.config["episodeSteps"]
         actual_steps = len(self.replay["steps"])
@@ -122,33 +201,55 @@ class HaliteV2(object):
         return actual_steps
 
     def find_winner(self):
-        assert(self.replay is not None)
-        assert(self.replay["steps"][-1] is not None)
+        """
+        Find the id of the player who gets most reward at the last of the game
+        :return:
+        """
+        assert (self.replay is not None)
+        assert (self.replay["steps"][-1] is not None)
         player_reward = []
         last_step = self.replay["steps"][- 1]
         first_step = self.replay["steps"][1]
         # has to be 4 player assume
-        assert(len(last_step) == len(first_step))
+        assert (len(last_step) == len(first_step))
         for player in last_step:
             player_reward.append(player["reward"])
         return player_reward.index(max(player_reward))
 
     def convert_to_game_play(self, step: int):
         """
-        Convert a step of game play (observation on the json) to the format that supports by halite helper function
+        Convert a step of replay from json file to the format that supports by halite helper function
+        and can be used directly in the real time game play
         :return:
         """
-        assert(self.replay is not None)
-        assert(self.config is not None)
-        assert(0 < step <= self.total_turns - 1)
+        assert (self.replay is not None)
+        assert (self.config is not None)
+        assert (0 < step <= self.total_turns - 1)
         observation = self.replay["steps"][step - 1][0]["observation"]
         board = Board(observation, self.config)
-        assert(board.current_player_id == 0)
+        assert (board.current_player_id == 0)
         if self.winner_id != 0:
             return None
         ### !!!!!!! Currently we only study the game where player 0 is the final winner
-        assert(board.current_player_id == self.winner_id == 0)
+        assert (board.current_player_id == self.winner_id == 0)
         return Gameplay(observation, self.config)
+
+    def build_game_play_list(self):
+        """
+        Convert every steps of replay to a list of "Gameplay" object that supports by kaggle helper function
+        :return:
+        """
+        assert (self.replay is not None)
+        assert (self.config is not None)
+        game_play_list = []
+        for step, _ in enumerate(self.replay["steps"]):
+            if step == 0:
+                continue
+            game_play_list.append(self.convert_to_game_play(step))
+        assert (len(game_play_list) == self.total_turns - 1)
+        return game_play_list
+
+
 class Halite(object):
     """
 
@@ -196,13 +297,13 @@ class Halite(object):
         self.turns_left = np.array([total_step - i for i in range(total_step)])
 
     def find_winner(self):
-        assert(self.replay is not None)
-        assert(self.replay["steps"][-1] is not None)
+        assert (self.replay is not None)
+        assert (self.replay["steps"][-1] is not None)
         player_reward = []
         last_step = self.replay["steps"][- 1]
         first_step = self.replay["steps"][1]
         # has to be 4 player assume
-        assert(len(last_step) == len(first_step))
+        assert (len(last_step) == len(first_step))
         for player in last_step:
             player_reward.append(player["reward"])
         return player_reward.index(max(player_reward))
@@ -236,8 +337,8 @@ class Halite(object):
         assert (halite.shape[0] == total_step - 1)
         assert (halite.shape[1] == map_size)
         assert (halite.shape[2] == map_size)
-        halite = halite/100  # round it
-       # print(halite)
+        halite = halite / 100  # round it
+        # print(halite)
         return halite
 
     def load_moves(self, map_size: int, num_of_players: int):
@@ -272,7 +373,7 @@ class Halite(object):
 
                 # load player 0's information
                 # TODO: change it to all players
-                assert(self.winner != -1)
+                assert (self.winner != -1)
                 if player_id == self.winner:
                     player_observation = observation["players"][player_id]
                     # Get halite, shipyard, ship information of the player
@@ -331,8 +432,6 @@ class Halite(object):
             step_shipyard_position = np.zeros((map_size, map_size), np.int32)
             step_ship_cargo = np.zeros((map_size, map_size), np.float32)
 
-
-
             # Load ship moves for all active players
             for pid in range(len(content)):
                 if "player" not in content[pid]["observation"]:
@@ -341,7 +440,7 @@ class Halite(object):
 
                 # load player 0's information
                 # TODO: change it to all players
-                assert(self.winner != -1)
+                assert (self.winner != -1)
                 player_observation = observation["players"][player_id]
                 # Get halite, shipyard, ship information of the player
                 player_shipyard = player_observation[1]
@@ -356,15 +455,16 @@ class Halite(object):
 
                 # load shipyard position
                 for shipyard_id, shipyard_info in player_shipyard.items():
-                    assert(isinstance(shipyard_info, int)) # shipyard_info : pos
+                    assert (isinstance(shipyard_info, int))  # shipyard_info : pos
                     shipyard_pos_1d = shipyard_info
                     shipyard_pos_2d = geometry.get_2D_col_row(map_size, shipyard_pos_1d)
-                    step_shipyard_position[shipyard_pos_2d[0]][shipyard_pos_2d[1]] = 2 if player_id == self.winner else 1
+                    step_shipyard_position[shipyard_pos_2d[0]][
+                        shipyard_pos_2d[1]] = 2 if player_id == self.winner else 1
             shipyard_position.append(step_shipyard_position)
             ships_position.append(step_ships_position)
             ships_cargo.append(step_ship_cargo)
         ships_position = np.array(ships_position)
-        ships_cargo = np.array(ships_cargo)/100
+        ships_cargo = np.array(ships_cargo) / 100
         shipyard_position = np.array(shipyard_position)
         return ships_position, ships_cargo, shipyard_position
 
@@ -375,7 +475,7 @@ class Halite(object):
         :return:
         """
         assert (self.replay is not None and self.halite is not None)
-        #print(self.halite.shape)
+        # print(self.halite.shape)
         # print(self.ship_actions.shape)
         assert (self.halite.shape[0] == self.ship_actions.shape[0])
         step = self.halite.shape[0]
@@ -409,7 +509,6 @@ class Halite(object):
             ship_info[:, pad_offset:pad_offset + shape, pad_offset:pad_offset + shape] = self.ship_position
             return ship_info
         return self.ship_position.copy()
-
 
 # game = Halite()
 # game.load_replay("1208740.json")
--- train_v2.py
+++ train_v2.py
@@ -21,7 +21,7 @@ for f in replay_files:
     print(f)
 
 batch_size = 16  # Batch size for training.
-epochs = 1  # Number of epochs to train for.
+epochs = 10  # Number of epochs to train for.
 latent_dim = 32  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 
