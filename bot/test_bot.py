daab6fe45f87e01e76651f7cad1745004253b0b1
russell tian <russell.tian1996@gmail.com>
2020-07-23 15:24:52 -0400

    Revamp util function to avoid duplicate work - the agent is defined in the 'Gameplay' class with real time config and observation of board - in training mode, we retrieve the information of each step from json file and convert that into a 'Gameplay' object... - The Gameplay object is meant to be the base class for game combat, any changes regarding to model and be derived from gameplay. - Add corresponding unit tests

--- bot/test_bot.py
+++ bot/test_bot.py
@@ -1,16 +1,15 @@
 from kaggle_environments import evaluate, make
 import sys
-from bot import v1_bot
+from bot import rnn_bot
 from bot import t_bot
-env = make("halite", configuration={"size": 5, "episodeSteps": 20}, debug=True)
+env = make("halite", configuration={"size": 21, "episodeSteps": 20}, debug=True)
 trainer = env.train([None, "random"])
 observation = trainer.reset()
 mybot = None
 turn = 0
 while not env.done:
     if not mybot:
-        mybot = v1_bot.Gameplay(observation, env.configuration)
-        mybot.load_model()
+        mybot = rnn_bot.Gameplay(observation, env.configuration)
     else:
         mybot.reset_board(observation, env.configuration)
     action = mybot.agent(observation, env.configuration)
@@ -19,7 +18,7 @@ while not env.done:
     observation = trainer.step(action)[0]
     print("Reward gained", observation.players[0][0])
     print("Turn", turn)
-    turn +=1
+    turn += 1
 out = env.render(mode="html", width=500, height=450)
 f = open("replay.html", "wt")
 f.write(out)
--- train/tests/test_data_utils.py
+++ train/tests/test_data_utils.py
@@ -3,36 +3,55 @@ import unittest
 
 class testHalite(unittest.TestCase):
 
-    def test_load_replay(self):
-        file = "replayJson.json"
-        game = utils.Halite()
-        game.load_replay(file)
-        self.assertIsNotNone(game.replay)
-
-    def test_load_data(self):
-        file = "replayJson.json"
-        game = utils.Halite()
-        game.load_replay(file)
-        game.load_data()
-        self.assertIsNot(game.halite, [])
-        self.assertIsNot(game.ship_actions, [])
-        self.assertIsNot(game.shipyard_actions, [])
-
-    def test_custom(self):
-        file = "replayJson.json"
-        game = utils.Halite()
-        game.load_replay(file)
-        game.load_data()
-        length = len(game.halite)
-        for i in range(length):
-            print(game.ship_position[i])
-            print(game.ship_actions[i], " \n")
-
+    # def test_load_replay(self):
+    #     file = "replayJson.json"
+    #     game = utils.Halite()
+    #     game.load_replay(file)
+    #     self.assertIsNotNone(game.replay)
+    #
+    # def test_load_data(self):
+    #     file = "replayJson.json"
+    #     game = utils.Halite()
+    #     game.load_replay(file)
+    #     game.load_data()
+    #     self.assertIsNot(game.halite, [])
+    #     self.assertIsNot(game.ship_actions, [])
+    #     self.assertIsNot(game.shipyard_actions, [])
+    #
+    # def test_custom(self):
+    #     file = "replayJson.json"
+    #     game = utils.Halite()
+    #     game.load_replay(file)
+    #     game.load_data()
+    #     length = len(game.halite)
+    #     for i in range(length):
+    #         print(game.ship_position[i])
+    #         print(game.ship_actions[i], " \n")
+    #
     def test_find_winner(self):
-        assert([1,2,3,4].index(max([1,2,3,4])) == 3)
+        assert([1, 2, 3, 4].index(max([1, 2, 3, 4])) == 3)
         assert ([1, 5, 3, 4].index(max([1, 5, 3, 4])) == 1)
         assert ([1, 2, 12, 4].index(max([1, 2, 12, 4])) == 2)
         assert ([10, 2, 3, 4].index(max([10, 2, 3, 4])) == 0)
+
+    def test_load_replay_v2(self):
+        file = "replayJson.json"
+        game = utils.HaliteV2(file)
+        self.assertIsNotNone(game.replay)
+        self.assertIsNotNone(game.config)
+
+    def test_load_total_turns(self):
+        file = "replayJson.json"
+        game = utils.HaliteV2(file)
+        self.assertGreater(game.total_turns, 1)
+        self.assertLessEqual(game.total_turns, 400)
+
+    def test_convert_to_gameplay(self):
+        file = "replayJson.json"
+        game = utils.HaliteV2(file)
+        gameplay = game.convert_to_game_play(2)
+        self.assertIsNotNone(gameplay)
+
 if __name__ == '__main__':
     unittest.main()
 
